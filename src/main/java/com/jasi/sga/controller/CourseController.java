5a06f0837f57836eacb3cf268622b2a86df9af2d
Jacinto Escobar <eqjo190657@upemor.edu.mx>
2024-04-18 16:09:36 -0700

    put course endpoint finished

--- src/main/java/com/jasi/sga/controller/CourseController.java
+++ src/main/java/com/jasi/sga/controller/CourseController.java
@@ -1,5 +1,6 @@
 package com.jasi.sga.controller;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.NoSuchElementException;
 
@@ -11,12 +12,13 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.jasi.sga.dto.CreateCourseRequest;
+import com.jasi.sga.dto.CreateUpdateCourseRequest;
 import com.jasi.sga.model.Course;
 import com.jasi.sga.service.CourseService;
 import com.jasi.sga.util.RequestValidator;
@@ -54,7 +56,7 @@ public class CourseController {
     }
 
     @PostMapping(path = "/create")
-    public ResponseEntity<?> createCourse(@RequestBody @Valid CreateCourseRequest createCourseRequest, BindingResult validation) {
+    public ResponseEntity<?> createCourse(@RequestBody @Valid CreateUpdateCourseRequest createCourseRequest, BindingResult validation) {
         if (RequestValidator.isInvalid(validation)) {
             return new ResponseEntity<>(ResponseBuilder.buildErrorResponse(SgaErrorMessage.INVALID_INFORMATION, SgaErrorCode.INVALID_INFORMATION, RequestValidator.getAllErrors(validation)), HttpStatus.BAD_REQUEST);
         }
@@ -79,5 +81,22 @@ public class CourseController {
         }
         courseService.delete(currentCourse);
         return new ResponseEntity<>(ResponseBuilder.buildSuccessResponse(SgaResponseMessage.ELEMENT_DELETED, currentCourse), HttpStatus.OK);
+    }
+
+    @PutMapping("/update/{courseId:^[0-9]*$}")
+    public ResponseEntity<?> putUpdateCourse(@PathVariable Long courseId, @RequestBody @Valid CreateUpdateCourseRequest updateCourseRequest, BindingResult validation) {
+        if (RequestValidator.isInvalid(validation)) {
+            return new ResponseEntity<>(ResponseBuilder.buildErrorResponse(SgaErrorMessage.INVALID_INFORMATION, SgaErrorCode.INVALID_INFORMATION, RequestValidator.getAllErrors(validation)), HttpStatus.BAD_REQUEST);
+        }
+
+        Course currentCourse = null;
+        try {
+            currentCourse = courseService.getCourseById(courseId);
+        } catch (NoSuchElementException ex) {
+            return new ResponseEntity<>(ResponseBuilder.buildErrorResponse(SgaErrorMessage.ELEMENT_NOT_FOUND, SgaErrorCode.ELEMENT_NOT_FOUND, SgaResponseMessage.NO_ELEMENT_FOUND), HttpStatus.NOT_FOUND);
+        }
+        
+        Course modifiedCourse = courseService.putUpdate(currentCourse, updateCourseRequest);
+        return new ResponseEntity<>(ResponseBuilder.buildSuccessResponse(SgaResponseMessage.COMPLETE_ELEMENT_UPDATE, modifiedCourse), HttpStatus.OK);
     }
 }
--- src/main/java/com/jasi/sga/dto/CreateUpdateCourseRequest.java
+++ src/main/java/com/jasi/sga/dto/CreateUpdateCourseRequest.java
@@ -6,7 +6,7 @@ import lombok.Data;
 
 @Data
 @AllArgsConstructor
-public class CreateCourseRequest {
+public class CreateUpdateCourseRequest {
     @NotBlank
     private String name;
 
